1948e3781b9a43a1a4ba84f791d8c164c5e8bc81
Remy GENY <rgeny@e2r2p9.clusters.42paris.fr>
2022-01-01 12:57:58 +0100

    echo corrigé

--- srcs/builtin/builtin_echo.c
+++ srcs/builtin/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: rgeny <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/31 18:20:42 by rgeny             #+#    #+#             */
-/*   Updated: 2021/12/31 19:03:20 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/01/01 12:57:17 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,36 @@
 #include "str.h"
 #include "builtin.h"
 
-static t_echo	static_flag(int argc, char *flag)
+static t_echo	static_flag(int argc, char *argv[])
 {
-	if (argc > 1 && !str_cmp("-n", flag))
-		return (FLAG_N);
-	return (FLAG_NO);
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (argv[i][0] == '-')
+		{
+			j = 1;
+			while (argv[i][j] == 'n')
+				j++;
+			if (argv[i][j])
+				return (i);
+		}
+		else
+			return (i);
+		i++;
+	}
+	return (i);
 }
 
 int	main(int argc, char *argv[])
 {
-	t_echo	echo;
+	int		ret;
 	int		i;
 
-	echo = static_flag(argc, argv[1]);
-	if (echo == FLAG_NO)
-		i = 1;
-	else
-		i = 2;
+	ret = static_flag(argc, argv);
+	i = ret;
 	while (i < argc)
 	{
 		write(1, argv[i], str_len(argv[i], 0));
@@ -38,7 +51,7 @@ int	main(int argc, char *argv[])
 		if (i < argc)
 			write(1, " ", 1);
 	}
-	if (echo == FLAG_NO)
+	if (ret == 1)
 		write(1, "\n", 1);
 	return (0);
 }
