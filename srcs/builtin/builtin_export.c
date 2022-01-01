fbcdd3e57335e2ad6cdd0c711f2af0240cb3b79a
Remy GENY <rgeny@e2r2p9.clusters.42paris.fr>
2022-01-01 15:17:12 +0100

    export correction

--- srcs/builtin/builtin_export.c
+++ srcs/builtin/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: rgeny <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 18:35:26 by rgeny             #+#    #+#             */
-/*   Updated: 2022/01/01 11:14:08 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/01/01 15:15:14 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ static void	static_sort(char **s)
 	}
 }
 
-static void	static_print(t_env *env)
+static int	static_print(t_env *env)
 {
 	char	**cpy;
 	int		i;
@@ -54,29 +54,102 @@ static void	static_print(t_env *env)
 		i++;
 	}
 	str_free_string(cpy);
+	return (0);
 }
 
-static void	static_new(char **cmd, t_env **env)
+static int	static_isdigitchar(char c)
 {
-	int		i;
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_')
+		return (1);
+	if (c >= '0' && c <= '9')
+		return (2);
+	return (0);
+}
+
+static int	static_new(char **cmd, t_env **env)
+{
+	int		j;
 	char	**var;
 
-	i = 1;
-	while (cmd[i])
+	j = 0;
+	while (cmd[0][j] && cmd[0][j] != '=')
 	{
-		var = str_split(cmd[i], "=");
-		env_new(env, str_ndup(var[0], str_len(var[0], 0)),
-			str_ndup(var[1], str_len(var[1], 0)));
-		str_free_string(var);
-		i++;
+		if (!static_isdigitchar(cmd[0][j]))
+		{
+			str_printerr("minishell: export: '", cmd[0],
+				"': not a valid identifier\n", 0);
+			return (1);
+		}
+		j++;
 	}
+	var = str_split(cmd[0], "=");
+	env_new(env, str_ndup(var[0], str_len(var[0], 0)),
+		str_ndup(var[1], str_len(var[1], 0)));
+	str_free_string(var);
+	return (0);
 }
 
 int	builtin_export(char **cmd, t_env **env)
 {
+	int	i;
+	int	ret;
+
+	ret = 0;
 	if (!cmd[1])
-		static_print(*env);
+		return (static_print(*env));
 	else
-		static_new(cmd, env);
-	return (0);
+	{
+		i = 1;
+		ret = 0;
+		while (cmd[i])
+		{
+			if (static_isdigitchar(cmd[i][0]) == 1)
+				ret |= static_new(&cmd[i], env);
+			else
+			{
+				ret = 1;
+				str_printerr("minishell: export: '", cmd[i],
+					"': not a valid identifier\n", 0);
+			}
+			i++;
+		}
+	}
+	return (ret);
 }
+/*	int		i;
+	int		j;
+	int		ret;
+	char	**var;
+
+	i = 1;
+	ret = 0;
+	while (cmd[i])
+	{
+		j = 0;
+		if (static_isdigitchar(cmd[i][j]) == 1)
+		{
+			while (cmd[i][j] && cmd[i][j] != '=')
+			{
+				if (!static_isdigitchar(cmd[i][j]))
+				{
+					str_printerr("minishell: export: '", cmd[i],
+						"': not a valid identifier\n", 0);
+					ret = 1;
+					break ;
+				}
+				j++;
+			}
+			var = str_split(cmd[i], "=");
+			env_new(env, str_ndup(var[0], str_len(var[0], 0)),
+				str_ndup(var[1], str_len(var[1], 0)));
+			str_free_string(var);
+		}
+		else
+		{
+			ret = 1;
+			str_printerr("minishell: export: '", cmd[i],
+				"': not a valid identifier\n", 0);
+		}
+		i++;
+	}
+	return (ret);*/
