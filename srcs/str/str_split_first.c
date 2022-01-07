1b4e71c286d46a3a7f95491bc80f979399d9550a
rgeny <remy.geny@gmail.com>
2022-01-07 19:26:27 +0100

    correction export A=

--- srcs/str/str_split_first.c
+++ srcs/str/str_split_first.c
@@ -6,7 +6,7 @@
 /*   By: rgeny <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/01 18:17:27 by rgeny             #+#    #+#             */
-/*   Updated: 2022/01/01 18:26:43 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/01/07 19:23:59 by buschiix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	**str_split_first(char *src, char c)
 	len = str_len(src, c);
 	ret[0] = str_ndup(src, len);
 	ret[1] = 0;
-	if (src[len] && src[len + 1])
+	if (src[len])
 		ret[1] = str_ndup(&src[len + 1], str_len(&src[len + 1], 0));
 	ret[2] = 0;
 	return (ret);
