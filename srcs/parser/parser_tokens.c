e04829807afde9d57b409728f8511692fdf05275
tonykino <tony.kino.3@gmail.com>
2022-01-30 17:41:09 +0100

    extract #_set_root from #parse_tokens

--- srcs/parser/parser_tokens.c
+++ srcs/parser/parser_tokens.c
@@ -6,7 +6,7 @@
 /*   By: tokino <tokino@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 12:21:43 by tokino            #+#    #+#             */
-/*   Updated: 2022/01/30 17:25:07 by tokino           ###   ########.fr       */
+/*   Updated: 2022/01/30 17:41:50 by tokino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 bool	is_command_token(t_token_type type)
 {
 	return (type == E_TOKEN_TYPE_WORD || type == E_TOKEN_TYPE_REDIRECTION);
+}
+
+static void	_set_root(t_data *data, t_node *n_command, t_node *n_separator)
+{
+	if (n_separator)
+		data->ast_root = n_separator;
+	else
+		data->ast_root = n_command;
 }
 
 int	parse_tokens(t_data *data, t_token *tokens)
@@ -26,22 +34,21 @@ int	parse_tokens(t_data *data, t_token *tokens)
 
 	current_token = tokens;
 	n_separator = NULL;
-	if (ret = init_n_command(&current_token, &n_command, n_separator))
-		return (print_parser_error(data, ret, current_token)); // TODO - Manage malloc error
+	ret = init_n_command(&current_token, &n_command, n_separator);
+	if (ret)
+		return (print_parser_error(data, ret, current_token));
 	while (current_token)
 	{
-
-		if (ret = init_n_separator(&n_separator, n_command))
-			return (print_parser_error(data, ret, current_token)); // TODO - Manage malloc error
+		ret = init_n_separator(&n_separator, n_command);
+		if (ret)
+			return (print_parser_error(data, ret, current_token));
 		if (!current_token->next)
-			return (print_parser_error(data, 1, current_token)); // TODO - Manage malloc error
+			return (print_parser_error(data, 1, current_token));
 		current_token = current_token->next;
-		if (ret = init_n_command(&current_token, &n_command, n_separator))
-			return (print_parser_error(data, ret, current_token)); // TODO - Manage malloc error
+		ret = init_n_command(&current_token, &n_command, n_separator);
+		if (ret)
+			return (print_parser_error(data, ret, current_token));
 	}
-	if (n_separator)
-		data->ast_root = n_separator;
-	else
-		data->ast_root = n_command;
+	_set_root(data, n_command, n_separator);
 	return (OK);
 }
