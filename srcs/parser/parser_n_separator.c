d39cc34ca09b6dc3bb33039fe065e4bcfa5e1b3b
tonykino <tony.kino.3@gmail.com>
2022-02-06 13:30:03 +0100

    move #init_andor_node

--- srcs/parser/parser_n_separator.c
+++ srcs/parser/parser_n_separator.c
@@ -6,7 +6,7 @@
 /*   By: tokino <tokino@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 11:49:27 by tokino            #+#    #+#             */
-/*   Updated: 2022/02/06 12:06:11 by tokino           ###   ########.fr       */
+/*   Updated: 2022/02/06 12:10:59 by tokino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,23 @@ void	init_n_pipe(t_node **pipe_node, t_node *n_command)
 	else
 		new_n_separator->left = *pipe_node;
 	*pipe_node = new_n_separator;
+}
+
+t_node	*init_andor_node(t_token *token, t_node *andor_node, t_node *pipeline_node)
+{
+	t_node	*new_andor_node;
+	t_node_type	type;
+	
+	if (token->type == E_TOKEN_TYPE_OR)
+		type = E_NODE_TYPE_OR;
+	else //if (token->type == E_TOKEN_TYPE_AND)
+		type = E_NODE_TYPE_AND;
+	new_andor_node = n_create(type);
+	if (new_andor_node == NULL)
+		return (NULL) ;
+	if (!andor_node)
+		new_andor_node->left = pipeline_node;
+	else
+		new_andor_node->left = andor_node;
+	return (new_andor_node);
 }
--- srcs/parser/parser_tokens.c
+++ srcs/parser/parser_tokens.c
@@ -6,7 +6,7 @@
 /*   By: tokino <tokino@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 12:21:43 by tokino            #+#    #+#             */
-/*   Updated: 2022/02/06 12:07:33 by tokino           ###   ########.fr       */
+/*   Updated: 2022/02/06 12:11:05 by tokino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,25 +52,6 @@ t_node	*init_pipeline(t_token **token)
 			print_syntax_error(*token); // tokens finished by a pipe
 	}
 	return (_set_root(command_node, pipe_node));
-}
-
-t_node	*init_andor_node(t_token *token, t_node *andor_node, t_node *pipeline_node)
-{
-	t_node	*new_andor_node;
-	t_node_type	type;
-	
-	if (token->type == E_TOKEN_TYPE_OR)
-		type = E_NODE_TYPE_OR;
-	else //if (token->type == E_TOKEN_TYPE_AND)
-		type = E_NODE_TYPE_AND;
-	new_andor_node = n_create(type);
-	if (new_andor_node == NULL)
-		return (NULL) ;
-	if (!andor_node)
-		new_andor_node->left = pipeline_node;
-	else
-		new_andor_node->left = andor_node;
-	return (new_andor_node);
 }
 
 t_node	*init_pipeline_list(t_token **tokens)
