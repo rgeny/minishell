33977555fd785db4c8a5db0b873a117df8085ce7
tonykino <tony.kino.3@gmail.com>
2022-02-07 14:09:58 +0100

    refactor #init_pipeline

--- srcs/parser/parser_init_pipeline.c
+++ srcs/parser/parser_init_pipeline.c
@@ -6,11 +6,21 @@
 /*   By: tokino <tokino@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 13:42:43 by tokino            #+#    #+#             */
-/*   Updated: 2022/02/07 13:10:54 by tokino           ###   ########.fr       */
+/*   Updated: 2022/02/07 14:08:51 by tokino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+
+static bool	_is_valid_token(t_token *token)
+{
+	t_token_type	type;
+
+	if (!token)
+		return (false);
+	type = token->type;
+	return (is_pipeline_token(token) || type == E_TOKEN_TYPE_PARENTHESIS_OPEN);
+}
 
 static t_node	*_init_pipe_node(t_node *pipe_node, t_node *command_node)
 {
@@ -26,22 +36,33 @@ static t_node	*_init_pipe_node(t_node *pipe_node, t_node *command_node)
 	return (new_pipe_node);
 }
 
+static t_node	*_init_right_child(t_token **tokens)
+{
+	t_node	*right_child;
+
+	right_child = NULL;
+	if (!is_error() && _is_valid_token((*tokens)->next))
+	{
+		*tokens = (*tokens)->next;
+		if (is_pipeline_token(*tokens))
+			right_child = init_command(tokens);
+		else if (is_opened_parenthesis_token(*tokens))
+		{
+			*tokens = (*tokens)->next;
+			right_child = init_pipeline_list(tokens, true);
+		}
+	}
+	else
+		print_syntax_error(*tokens);
+	return (right_child);
+}
+
 t_node	*_set_pipeline_root(t_node *main_node, t_node *separator_node)
 {
 	if (separator_node)
 		return (separator_node);
 	else
 		return (main_node);
-}
-
-static bool	_is_valid_token(t_token *token)
-{
-	t_token_type type;
-
-	if (!token)
-		return (false);
-	type = token->type;
-	return (is_pipeline_token(token) || type == E_TOKEN_TYPE_PARENTHESIS_OPEN);
 }
 
 t_node	*init_pipeline(t_token **tokens)
@@ -59,25 +80,11 @@ t_node	*init_pipeline(t_token **tokens)
 	pipe_node = NULL;
 	command_node = init_command(tokens);
 	if (is_opened_parenthesis_token(*tokens))
-		print_syntax_error(*tokens); 
+		print_syntax_error(*tokens);
 	while (!is_error() && _is_valid_token(*tokens))
 	{
 		pipe_node = _init_pipe_node(pipe_node, command_node);
-		if (!is_error() && _is_valid_token((*tokens)->next))
-		{
-			*tokens = (*tokens)->next;
-			if (is_pipeline_token(*tokens))
-				pipe_node->right = init_command(tokens);
-			else if (is_opened_parenthesis_token(*tokens))
-			{
-				// printf("( spotted !!\n");
-				*tokens = (*tokens)->next;
-				pipe_node->right = init_pipeline_list(tokens, true);
-			}
-		}
-		else // if ((*token)->next == NULL)
-			print_syntax_error(*tokens); // tokens finished by a pipe
-
+		pipe_node->right = _init_right_child(tokens);
 	}
 	return (_set_pipeline_root(command_node, pipe_node));
 }
