0aa049cd77276e273e0b03a8ded82ceb67316d8e
Remy GENY <rgeny@e2r2p16.clusters.42paris.fr>
2022-02-10 15:59:26 +0100

    rebuild exe

--- srcs/exe/exe_cmd.c
+++ srcs/exe/exe_cmd.c
@@ -6,27 +6,11 @@
 /*   By: tokino <tokino@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 10:08:26 by rgeny             #+#    #+#             */
-/*   Updated: 2022/02/10 14:55:21 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/02/10 15:58:18 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exe.h"
-#include <stdio.h>
-#include "error.h"
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include "str.h"
-
-void	_print(char **s)
-{
-	while (*s)
-	{
-		printf("%s\n", *s);
-		s++;
-	}
-}
 
 void	exe_cmd(t_ast *ast, t_data *data)
 {
@@ -34,6 +18,7 @@ void	exe_cmd(t_ast *ast, t_data *data)
 	int		arg_n;
 
 	arg_n = 0;
+	args = NULL;
 	exe_redir(ast->cmd, ast->cmd->redirections, ast->cmd->fd_heredoc, data);
 	if (!is_error())
 	{
@@ -45,10 +30,10 @@ void	exe_cmd(t_ast *ast, t_data *data)
 		exe_builtin(args, &args[1], data);
 		if (g_last_return == -1)
 			exe_out_process(ast->cmd, args, data);
-		str_free_list(&args);
 	}
 	if (is_error())
 		g_last_return = error_get();
+	str_free_list(&args);
 	ast->cmd->last_return = g_last_return;
 	error_reset();
 }
