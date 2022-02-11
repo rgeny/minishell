130a6a73c83b77e089939a86dc5dc37d99a42419
tonykino <tony.kino.3@gmail.com>
2022-02-11 16:49:29 +0100

    fix ^\signal withfork

--- srcs/signal/signal_ignore.c
+++ srcs/signal/signal_ignore.c
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   signal_ignore.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: buschiix <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tokino <tokino@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 00:56:29 by buschiix          #+#    #+#             */
-/*   Updated: 2022/02/11 16:33:27 by tokino           ###   ########.fr       */
+/*   Updated: 2022/02/11 16:49:05 by tokino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_signal.h"
 
-void	_sigint_nl(int sig)
+void	_sig_nl(int sig)
 {
 	write(1, "\n", 1);
 }
 
 void	signal_ignore(void)
 {
-	signal(SIGINT, _sigint_nl);
-	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, _sig_nl);
+	signal(SIGQUIT, _sig_nl);
 }
