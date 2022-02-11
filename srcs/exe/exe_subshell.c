5d5a6aa946159dc451846e5414d8435ac53055a9
Remy GENY <rgeny@e2r2p15.clusters.42paris.fr>
2022-02-11 14:46:53 +0100

    leak subshell

--- srcs/exe/exe_subshell.c
+++ srcs/exe/exe_subshell.c
@@ -6,7 +6,7 @@
 /*   By: rgeny <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 03:48:59 by rgeny             #+#    #+#             */
-/*   Updated: 2022/02/10 03:50:28 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/02/11 14:46:30 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 void	exe_subshell(t_ast *ast, t_data *data)
 {
 	exe_main(ast, data);
+	env_del_all(data->env);
+	free_ast(&data->ast);
 	exit(g_last_return);
 }
