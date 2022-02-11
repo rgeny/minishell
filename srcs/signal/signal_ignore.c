eae48f19becb74e809e9ebffc6a4c1ce989f8688
tonykino <tony.kino.3@gmail.com>
2022-02-11 16:37:39 +0100

    fix newline return with forked ^C

--- srcs/signal/signal_ignore.c
+++ srcs/signal/signal_ignore.c
@@ -6,14 +6,19 @@
 /*   By: buschiix <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 00:56:29 by buschiix          #+#    #+#             */
-/*   Updated: 2022/02/10 18:38:07 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/02/11 16:33:27 by tokino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_signal.h"
 
+void	_sigint_nl(int sig)
+{
+	write(1, "\n", 1);
+}
+
 void	signal_ignore(void)
 {
-	signal(SIGINT, SIG_IGN);
+	signal(SIGINT, _sigint_nl);
 	signal(SIGQUIT, SIG_IGN);
 }
